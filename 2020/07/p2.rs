36d85041c9ce6ccc44b71dbf661e5f6f49558cc8
Anthony A Russo <aarusso.exe@outlook.com>
2021-08-02 22:54:27 -0700

    Began rework of Day 7 Part Two. Parsing text is nearly completed.

--- 2020/07/p2.rs
+++ 2020/07/p2.rs
@@ -3,67 +3,70 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
 
-fn count_children(parent: &str, map: &HashMap<String, String>, c_map: &mut HashMap<String, bool>) -> usize {
-    let mut child_count = 0;
-    // Create a separate (permanent) variable to store the parent string
-    // that parents' slices will borrow from.
-
-    // Child is NOT an outermost bag, i.e. has parent bags
-    if let Some(p) = map.get(parent) {
-        let child_string: String = p.to_string();
-        // Parse the string of parents into a Vector of Strings
-        let children: Vec<&str> = child_string
-            .split(';')
-            .collect();
-
-        for child in children.iter() {
-            // If this is the first time this particular parent
-            // has been accounted for, then increment.
-            if c_map.insert(child.to_string(), true).is_none() {
-                child_count += 1;
-                child_count += count_children(child, map, c_map);
-            }
+#[derive(Default)]
+struct Bag<'a> {
+    color: String,
+    // Vector of tuples. (Pointer, Amount)
+    contents: Vec<(&'a str, u32)>,
+}
+impl<'a> Bag<'a> {
+    pub fn new(color: &str) -> Self {
+        Self {
+            color: color.to_string(),
+            ..Default::default()
         }
     }
-
-    child_count
+    pub fn get_color(&self) -> &str {
+        &self.color
+    }
+    pub fn push(&mut self, color: &'a str, amount: u32) {
+        self.contents.push((color, amount));
+    }
 }
 
 fn main() -> io::Result<()> {
     let f = File::open("input.txt")?;
     let f = BufReader::new(f);
 
-    // <Key, Value> : <Bag Color, List of 'Parent' Bag Colors>
-    let mut bags_map: HashMap<String, String> = HashMap::new();
-    // <Key, Value> : <Bag Color, has_been_accounted>
-    let mut accounted_children: HashMap<String, bool> = HashMap::new();
+    // <Key, Value> : <Bag Color, List of Contents>
+    let mut bags_map: HashMap<&str, Bag> = HashMap::new();
 
     for line in f.lines() {
-       let line = line
-           .unwrap()
-           .to_string()
-           .replace(" bags", "")
-           .replace(" bag", "")
-           .replace(char::is_numeric, "")
-           .replace("  ", " ");
+        // line creates a String containing the bag names
+        if line.is_err() {
+            panic!("Error reading line!");
+        }
+        let line = line.unwrap();
 
-       let sundae = line.split_once("contain");
-       let parent = sundae.unwrap().0.trim_end();
+        if line.split_once(" contain ").is_none() {
+            panic!("Error: Cannot locate substring!");
+        }
+        let sundae = line.split_once(" contain ").unwrap();
+        let parent = sundae.0.trim_end_matches("bags").trim_end_matches("bag");
+        let children: Vec<&str> = sundae.1
+            .trim_end_matches('.')
+            .split(", ")
+            .map(|s| s
+                 .trim_start_matches(char::is_numeric)
+                 .trim_start()
+                 .trim_end_matches(" bags")
+                 .trim_end_matches(" bag"))
+            .collect();
 
-       let children: String = sundae.unwrap().1
-           .trim_end()
-           .replace('.', "")
-           .replace(", ", ";");
 
-       bags_map.insert(parent.to_string(), children);
+        print!("{} --- ", parent);
+        for child in children.iter() {
+            print!("{};", child);
+        }
+        print!("\n");
+
+        // map inserts using a reference to the String as a key
+        // if the bag doesnt exist in the map then the color field
+        // of the Bag struct permanently stores the color's name.
+        // If the bag does exist in map then update the Bag's
+        // contents field and/or set other Bags' contents to
+        // borrow from the Bag's color field.
     }
-
-    // bags_map now contains a list of every parent per child key
-    // in the form: "child", "parent1;parent2;parent3"
-    
-    accounted_children.reserve(bags_map.len());
-    println!("shiny gold bags contain {} other bags",
-             count_children("shiny gold", &bags_map, &mut accounted_children));
 
     Ok(())
 }
