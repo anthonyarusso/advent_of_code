52ea2624d79d6282b01d184f5c49f57dcb5bd21d
Anthony A Russo <aarusso.exe@outlook.com>
2021-08-04 16:26:00 -0700

    Reworked day 7 part 2 to use a pseudo-tree structure with a HashMap.

--- 2020/07/p2.rs
+++ 2020/07/p2.rs
@@ -3,27 +3,48 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
 
+const CHOSEN_BAG: &'static str = "shiny gold";
 type IndexSlice = (usize, usize);
 
 #[derive(Default)]
 struct Bag {
-    color: IndexSlice,
+    holders: Vec<IndexSlice>,
     // Vector of tuples. (Pointer, Amount)
     contents: Vec<(IndexSlice, u32)>,
 }
 impl Bag {
-    pub fn new(color: IndexSlice) -> Self {
-        Self {
-            color: color,
-            ..Default::default()
-        }
-    }
-    pub fn get_color(&self) -> IndexSlice {
-        self.color
-    }
-    pub fn push(&mut self, color: IndexSlice, amount: u32) {
+    pub fn push_content(&mut self, color: IndexSlice, amount: u32) {
         self.contents.push((color, amount));
     }
+}
+
+fn get_color_from_store(color: &str, store: &str) -> IndexSlice {
+    let start = store.find(color).expect("Could not find substring in store!");
+    let end = start + color.len();
+    (start, end)
+}
+
+fn count_unique_ancestors(color: IndexSlice, map: &HashMap<IndexSlice, Bag>) -> u32 {
+    // Fix this function. Currently is counts all parents of all ancestors,
+    // but does not check if any given ancestor is actually unique.
+    // Use something like a HashSet to resolve this.
+    let mut total = 0;
+    let child = map.get(&color).expect("Unable to locate key in map!");
+    for parent in child.holders.iter() {
+        total += 1;
+        total += count_unique_ancestors(*parent, map);
+    }
+    total
+}
+
+fn count_total_descendants(color: IndexSlice, map: &HashMap<IndexSlice, Bag>) -> u32 {
+    let mut total = 0;
+    let parent = map.get(&color).expect("Unable to locate key in map!");
+    for (child, amount) in parent.contents.iter() {
+        total += amount;
+        total += amount * count_total_descendants(*child, map);
+    }
+    total
 }
 
 fn main() -> io::Result<()> {
@@ -54,18 +75,20 @@ fn main() -> io::Result<()> {
         amounts.retain(|s| s.chars().all(char::is_numeric));
         let amounts: Vec<u32> = amounts
             .iter()
-            .map(|s| {s.parse::<u32>().expect("Failed to parse integer!") })
+            .map(|s| { s.parse::<u32>().expect("Failed to parse integer!") })
             .collect();
 
-        let p_start = store.len();
-        store.push_str(parent);
-        let p_end = store.len();
-        store.push(';');
-
-        // If the parent already exists in map, get a pointer to the pre-existing
-        // value. Otherwise, insert a new Bag an use its pointer.
-        let parent_bag = Bag::new((p_start, p_end));
-        let parent_bag = map.entry((p_start, p_end)).or_insert(parent_bag);
+        let p_start;
+        let p_end;
+        if let Some(ps) = store.find(parent) {
+            p_start = ps;
+            p_end = p_start + parent.len();
+        } else {
+            p_start = store.len();
+            store.push_str(parent);
+            p_end = p_start + parent.len();
+            store.push(';');
+        }
 
         // Not every child bag was given an amount. Either the input file
         // contains errors or lists the parent as containing "no other bags".
@@ -78,12 +101,16 @@ fn main() -> io::Result<()> {
             }
         }
 
+        let mut c_contents: Vec<(IndexSlice, u32)> = Vec::new();
+
+        // Push all child colors to store and save their indices
+        // to c_contents.
         if !no_other_bags {
-            for child in children.iter() {
+            for (i, child) in children.iter().enumerate() {
                 let start;
                 let end;
-                if store.contains(child) {
-                    start = store.find(child).expect("Cannot find substring!");
+                if let Some(cs) = store.find(child) {
+                    start = cs;
                     end = start + child.len();
                 } else {
                     start = store.len();
@@ -99,10 +126,36 @@ fn main() -> io::Result<()> {
                 // map.insert((start, end), Bag::new((start, end)));
 
                 // Update the parent bag's map entry with the child.
-                parent_bag.push((start, end), 0);
+                c_contents.push((
+                    (start, end),
+                    *amounts.get(i).expect("Amount not found!")));
             }
         }
-    }
+        // If the parent already exists in map, get a pointer to the pre-existing
+        // value. Otherwise, insert a new Bag an use its pointer.
+        let parent_bag = map.entry((p_start, p_end)).or_insert(Bag::default());
+
+        // Push child bags to parent's 'content' vector.
+        for (slice, amount) in c_contents.iter() {
+            parent_bag.push_content(*slice, *amount);
+        }
+
+        // Push the parent bag to each child's `holders` vector.
+        for (c_slice, _) in c_contents.iter() {
+            let c_bag = map.entry((c_slice.0, c_slice.1)).or_insert(Bag::default());
+            c_bag.holders.push((p_start, p_end));
+        }
+
+    } // end 'for line in lines'
+
+    let bag_of_interest = get_color_from_store(CHOSEN_BAG, &store);
+    println!("{} has {} unique ancestors.",
+             CHOSEN_BAG,
+             count_unique_ancestors(bag_of_interest, &map));
+
+    println!("Each {} bag contains {} other bags in total.",
+             CHOSEN_BAG,
+             count_total_descendants(bag_of_interest, &map));
 
     Ok(())
 }
