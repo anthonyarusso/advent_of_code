28a3a24f6cdd77b42ff8c505b25db2a77bcb3208
Anthony A Russo <aarusso.exe@outlook.com>
2021-12-16 15:34:32 -0800

    Added my solution to the Day Two problem.

---
+++ 2021/02/one.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int horizontal = 0, depth = 0;
+	string line, command;
+	int distance;
+	int space_index;
+	ifstream file;
+	file.open("input.txt");
+
+	if (file.is_open()) {
+		while (getline(file, line)) {
+			space_index = line.find(' ', 0);
+			command = line.substr(0, space_index);
+			distance = stoi(line.substr(space_index + 1, line.length()));
+
+			if (command == "forward") {
+				horizontal += distance;
+			} else if (command == "up") {
+				depth -= distance;
+			} else if (command == "down") {
+				depth += distance;
+			} else {
+				cout << "Command not recognized!" << endl;
+				break;
+			}
+		}
+
+		cout << "Horizontal: " << horizontal << " Depth: " << depth << endl;
+		cout << "Product: " << horizontal * depth << endl;
+
+		file.close();
+	} else {
+		cout << "Unable to open file." << endl;
+	}
+
+	return 0;
+}
