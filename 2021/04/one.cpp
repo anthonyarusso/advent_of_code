d4bc25119345627cde4af432d6adaf3705eeaaa3
Anthony A Russo <aarusso.exe@outlook.com>
2021-12-21 13:00:33 -0800

    Added work to 2021 Day 4, part one.

---
+++ 2021/04/one.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	vector<int> draws;
+	vector<int*> boards;
+	string line;
+	ifstream file;
+	file.open("input.txt");
+
+	if (file.is_open()) {
+		getline(file, line);
+		char* token;
+		// Convert line to char* for use in strtok.
+		char cstr[line.length() + 1];
+		strcpy(cstr, line.c_str());
+
+		token = strtok(cstr, ",");
+
+		while (token != NULL) {
+			draws.push_back(atoi(token));
+			token = strtok(NULL, ",");
+		}
+
+		char num_cstr[30];
+		int board_index = 0;
+		// Push a pointer to an empty int array to the boards vector.
+		boards.push_back(new int[25]);
+
+		// Fill the boards vector.
+		while (getline(file, line)) {
+			if (line == "") {
+				// Start a new board with an empty array.
+				boards.push_back(new int[25]);
+				board_index = 0;
+			} else {
+				// Push numbers to current board.
+				strcpy(num_cstr, line.c_str());
+				token = strtok(num_cstr, " ");
+
+				// Current board is at the end of the boards vector.
+
+				while (token != NULL) {
+					// Current board is at the end of the boards vector.
+					// I.e. boards.at(boards.size() - 1) is the current board.
+					boards.at(boards.size() - 1)[board_index] = atoi(token);
+					board_index += 1;
+					token = strtok(NULL, " ");
+				}
+			}
+		}
+	} else {
+		cout << "Failed to open file." << endl;
+	}
+
+	return 0;
+}
