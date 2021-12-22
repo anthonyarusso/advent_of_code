b2c1b5aa749453920196a1cc1bb44cad182aa97a
Anthony A Russo <aarusso.exe@outlook.com>
2021-12-22 12:36:18 -0800

    Added work to 2021 Day 4, part one.

--- 2021/04/one.cpp
+++ 2021/04/one.cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 
-#define BOARD_SIZE 25;
+#define BOARD_SIZE 25
 
 using namespace std;
+
+int draws_to_win(
+	   vector<int>& draws,
+	   vector<int>& marks,
+	   int* board,
+	   int row_size,
+	   int column_size
+	   );
 
 int main() {
 	vector<int> draws;
@@ -28,13 +36,13 @@ int main() {
 		char num_cstr[30];
 		int board_index = 0;
 		// Push a pointer to an empty int array to the boards vector.
-		boards.push_back(new int[25]);
+		boards.push_back(new int[BOARD_SIZE]);
 
 		// Fill the boards vector.
 		while (getline(file, line)) {
 			if (line == "") {
 				// Start a new board with an empty array.
-				boards.push_back(new int[25]);
+				boards.push_back(new int[BOARD_SIZE]);
 				board_index = 0;
 			} else {
 				// Push numbers to current board.
@@ -51,9 +59,86 @@ int main() {
 			}
 		}
 		// Boards vector is now filled.
+		vector<int> marks[boards.size()];
+		vector<int> draws_count;
+		int d;
+		for (int i = 0; i < boards.size(); i++) {
+			draws_count.push_back(
+					draws_to_win(
+						draws,
+						marks[i],
+						boards[i],
+						5,
+						5)
+					);
+		}
+		/*
+		for (auto board : boards) {
+			draws_count.push_back(draws_to_win(draws, board, 5, 5));
+		}
+		*/
+		int index = 0, winning_index = -1;
+		cout << "draws_count: ";
+		for (auto c : draws_count) {
+			cout << c << ", ";
+			if (c != -1) {
+				winning_index = index;
+				break;
+			}
+			index += 1;
+		}
+		cout << endl;
+		cout << "winning_index: " << winning_index << endl;
+		if (winning_index == -1) {
+			cout << "No winner found!" << endl;
+		} else {
+			cout << "Winner found!" << endl;
+			cout << "Draws to win: " << draws_count[winning_index] << endl;
+			cout << "Winner's Marks: ";
+			for (auto m : marks[winning_index]) {
+				cout << m << ", ";
+			}
+			cout << endl;
+		}
+
 	} else {
 		cout << "Failed to open file." << endl;
 	}
 
 	return 0;
+}
+
+int draws_to_win(
+		vector<int>& draws,
+		vector<int>& marks,
+		int* board,
+		int row_size,
+		int column_size
+		) {
+	int result = -1;
+	int board_size = row_size * column_size;
+	int rows[row_size] {0};
+	int columns[column_size] = {0};
+	
+	for (int i = 0; i < draws.size(); i++) {
+		for (int j = 0; j < board_size; j++) {
+			if (draws[i] == board[j]) {
+				// Increment our counters for rows and columns.
+				rows[j / row_size] += 1;
+				columns[j % row_size] += 1;
+				// Push index to marks.
+				marks.push_back(j);
+
+				if ((rows[j / row_size] == row_size) ||
+				(columns[j % row_size] == column_size)) {
+					result = i + 1;
+					goto result_found;
+				}
+			}
+		}
+	}
+
+	result_found:
+
+	return result;
 }
